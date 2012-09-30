bc3f2c6c090e88e5e08dd9a48d07da6f26be44ba
Misha Sakhnik <kskster@gmail.com>
2012-09-30 23:23:31 +0300

    auth

---
+++ fetch_photos.py
@@ -0,0 +1,80 @@
+import vk_auth
+import json
+import urllib2
+from urllib import urlencode
+import json
+import os
+import os.path
+import getpass
+import sys
+
+def call_api(method, params, token):
+    if isinstance(params, list):
+        params_list = [kv for kv in params]
+    elif isinstance(params, dict):
+        params_list = params.items()
+    else:
+        params_list = [params]
+    params_list.append(("access_token", token))
+    url = "https://api.vk.com/method/%s?%s" % (method, urlencode(params_list)) 
+    return json.loads(urllib2.urlopen(url).read())["response"]
+
+def get_albums(user_id, token):
+    return call_api("photos.getAlbums", ("uid", user_id), token)
+
+def get_photos_urls(user_id, album_id, token):
+    photos_list = call_api("photos.get", [("uid", user_id), ("aid", album_id)], token)
+    result = []
+    for photo in photos_list:
+        #Choose photo with largest resolution
+        if "src_xxbig" in photo:
+            url = photo["src_xxbig"]
+        elif "src_xbig" in photo:
+            url = photo["src_xbig"]
+        else:
+            url = photo["src_big"]
+        result.append(url)
+    return result
+
+def save_photos(urls, directory):
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+    names_pattern = "%%0%dd.jpg" % len(str(len(urls)))
+    for num, url in enumerate(urls):
+        filename = os.path.join(directory, names_pattern % (num + 1))
+        print "Downloading %s" % filename
+        open(filename, "w").write(urllib2.urlopen(url).read())
+
+def save_on_disk(albums):
+	print "\n".join("%d. %s" % (num + 1, album["title"]) for num, album in enumerate(albums))
+	choise = -1
+	while choise not in xrange(len(albums)):
+		choise = int(raw_input("Choose album number: ")) - 1
+	photos_urls = get_photos_urls(user_id, albums[choise]["aid"], token)
+	save_photos(photos_urls, directory)
+
+def get_users(user_id, token):
+	call_api("users.get", ("uid", user_id), token)
+
+if len(sys.argv) != 2:
+   print "Usage: %s destination" % sys.argv[0]
+   sys.exit(1)
+
+directory = sys.argv[1]
+email = raw_input("Email: ")
+password = getpass.getpass()
+token, user_id = vk_auth.auth(email, password, "2951857", "photos")
+print user_id;
+print token;
+users = [1, 2]
+users = get_users(user_id, token);
+print users;
+if users :
+	i = 0;
+	print len(users)
+else :
+	print "No"
+#user_id = 153174556;
+
+#albums = get_albums(user_id, token)
+#save_on_disk(albums)
